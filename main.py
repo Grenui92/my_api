8b40d5d91d13c9b4e0a6d78a89f58f521490f621
Grenui92 <grenui92@gmail.com>
2023-03-12 20:48:02 +0200

    async and birthdays

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from db import get_db
 from schemas import ContactResponse, ContactBase
 from models import Contact
+from datetime import date, timedelta
 
 app = FastAPI()
 
@@ -26,7 +27,7 @@ async def create_new_contact(contact_body: ContactBase,
 
 
 @app.get('/get_one_contact/{contact_id}', response_model=ContactResponse)
-def get_one_contact(contact_id: int, db: Session = Depends(get_db)):
+async def get_one_contact(contact_id: int, db: Session = Depends(get_db)):
     contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not found')
@@ -34,7 +35,7 @@ def get_one_contact(contact_id: int, db: Session = Depends(get_db)):
 
 
 @app.put('/update_contact/{contact_id}', response_model=ContactResponse)
-def update_contact(contact_body: ContactBase, id: int, db: Session = Depends(get_db)):
+async def update_contact(contact_body: ContactBase, id: int, db: Session = Depends(get_db)):
     contact = db.query(Contact).filter(Contact.id == id).first()
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not found')
@@ -51,14 +52,14 @@ def update_contact(contact_body: ContactBase, id: int, db: Session = Depends(get
 
 
 @app.delete('/delete_contact/{contact_id}', response_model=dict)
-def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+async def delete_contact(contact_id: int, db: Session = Depends(get_db)):
     db.query(Contact).filter(Contact.name == contact_id).delete()
     db.commit()
     return {'message': 'Contact was deleted'}
 
 
 @app.get('/search_by_name/{name}', response_model=ContactResponse)
-def search_by_name(name: str = 'Stas', db: Session = Depends(get_db)):
+async def search_by_name(name: str = 'Stas', db: Session = Depends(get_db)):
     contact = db.query(Contact).filter(Contact.name == name).first()
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not Found')
@@ -66,7 +67,7 @@ def search_by_name(name: str = 'Stas', db: Session = Depends(get_db)):
 
 
 @app.get('/search_by_surname/{surname}', response_model=ContactResponse)
-def search_by_surname(surname: str = 'Vasilenko', db: Session = Depends(get_db)):
+async def search_by_surname(surname: str = 'Vasilenko', db: Session = Depends(get_db)):
     contact = db.query(Contact).filter(Contact.surname == surname).first()
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not Found')
@@ -74,7 +75,7 @@ def search_by_surname(surname: str = 'Vasilenko', db: Session = Depends(get_db))
 
 
 @app.get('/search_by_email/{email}', response_model=ContactResponse)
-def search_by_email(email: str = 'grenui@gmail.com', db: Session = Depends(get_db)):
+async def search_by_email(email: str = 'grenui@gmail.com', db: Session = Depends(get_db)):
     contact = db.query(Contact).filter(Contact.email == email).first()
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not Found')
@@ -88,3 +89,12 @@ async def edit_contact(field: str, new_data: str, id: int = 3, db: Session = Dep
     db.commit()
     db.refresh(contact)
     return contact
+
+@app.get('/nearest_birthdays', response_model=list[ContactResponse])
+async def nearest_birthdays(db: Session = Depends(get_db)):
+
+    contacts = db.query(Contact).all()
+    contacts = [contact for contact in contacts
+                if abs(contact.birthday-date(contact.birthday.year, date.today().day, date.today().month))<timedelta(7)]
+
+    return contacts
